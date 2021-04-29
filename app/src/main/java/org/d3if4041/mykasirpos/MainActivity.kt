9ef9af27db08d2583904280e20a343d63dbad2fc
billyrefanto <mochbillyrefanto@gmail.com>
2021-04-29 15:09:14 +0700

    resolved error

--- app/src/main/java/org/d3if4041/mykasirpos/MainActivity.kt
+++ app/src/main/java/org/d3if4041/mykasirpos/MainActivity.kt
@@ -2,8 +2,11 @@ package org.d3if4041.mykasirpos
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.drawerlayout.widget.DrawerLayout
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.tambah_barang)
